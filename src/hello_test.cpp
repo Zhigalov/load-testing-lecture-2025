b51c0685121d6ee48c48e37df74d7ebc0cc74960
Sergey Zhigalov <sergey.zhigalov@gmail.com>
2025-05-08 12:48:01 +0000

    Set name `clck` instead `service_template`

--- src/hello_test.cpp
+++ src/hello_test.cpp
@@ -3,8 +3,8 @@
 #include <userver/utest/utest.hpp>
 
 UTEST(SayHelloTo, Basic) {
-  using service_template::SayHelloTo;
-  using service_template::UserType;
+  using clck::SayHelloTo;
+  using clck::UserType;
 
   EXPECT_EQ(SayHelloTo("Developer", UserType::kFirstTime),
             "Hello, Developer!\n");
