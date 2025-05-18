f2e7c4e05d632176a06f6f2a1d1ef46b37e987da
Sergey Zhigalov <sergey.zhigalov@gmail.com>
2025-05-18 11:55:10 +0000

    Add urls table

--- postgresql/schemas/db_1.sql
+++ postgresql/schemas/db_1.sql
@@ -1,8 +1,14 @@
 DROP SCHEMA IF EXISTS hello_schema CASCADE;
-
 CREATE SCHEMA IF NOT EXISTS hello_schema;
-
 CREATE TABLE IF NOT EXISTS hello_schema.users (
     name TEXT PRIMARY KEY,
     count INTEGER DEFAULT(1)
+);
+
+DROP SCHEMA IF EXISTS clck_schema CASCADE;
+CREATE SCHEMA IF NOT EXISTS clck_schema;
+CREATE TABLE IF NOT EXISTS clck_schema.urls (
+    hashsum TEXT NOT NULL,
+    vestion INTEGER DEFAULT(1),
+    url     TEXT NOT NULL
 );
